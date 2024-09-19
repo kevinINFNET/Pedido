b8373d19b610939837730d130c3d52e58cff2808
kevin <kevin@XDXDXD>
2024-09-19 10:03:32 -0300

    Adicionar arquivos de projeto.

---
+++ Pedido/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CalculadoraDePedido
+{
+  class Program
+{
+static void Main(string[] args)
+ {
+   Func<int, decimal, decimal, decimal, decimal> calcularValorTotal = (quantidade, precoUnitario, frete, desconto) =>
+            {
+ decimal valorItens = quantidade * precoUnitario;
+ decimal valorTotal = valorItens + frete - desconto;
+   return valorTotal < 0 ? 0 : valorTotal;
+  };
+ int quantidadeItens = 10;        
+   decimal precoUnitario = 77.00m; 
+   decimal frete = 17.00m;         
+   decimal desconto = 27.00m;      
+
+     decimal valorTotalPedido = calcularValorTotal(quantidadeItens, precoUnitario, frete, desconto);
+ Console.WriteLine($"Quantidade de Itens: {quantidadeItens}");
+ Console.WriteLine($"Preço Unitário: R$ {precoUnitario:F2}");
+ Console.WriteLine($"Frete: R$ {frete:F2}");
+ Console.WriteLine($"Desconto: R$ {desconto:F2}");
+ Console.WriteLine($"Valor Total do Pedido: R$ {valorTotalPedido:F2}");
+ Console.ReadLine();
+    }
+  }
+}
